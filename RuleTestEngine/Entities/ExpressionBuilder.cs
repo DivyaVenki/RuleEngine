c09e89abfcc5733d2ab2658939db915fe8ee3678
ch.narayana <ch.narayana@intlfcstone.com>
2018-03-15 21:28:03 +0530

    test

--- RuleTestEngine/Entities/ExpressionBuilder.cs
+++ RuleTestEngine/Entities/ExpressionBuilder.cs
@@ -10,102 +10,136 @@ using System.Threading.Tasks;
 
 namespace RuleTestEngine.Entities
 {
-    public class ExpressionBuilder
-    {
+	public class ExpressionBuilder
+	{
 
-        private static MethodInfo containsMethod = typeof(List<string>).GetMethod("Contains", new Type[] { typeof(string) });
-        private static MethodInfo startsWithMethod = typeof(string).GetMethod("StartsWith", new Type[] { typeof(string) });
-        private static MethodInfo endsWithMethod = typeof(string).GetMethod("EndsWith", new Type[] { typeof(string) });
+		private static MethodInfo containsMethod = typeof(List<string>).GetMethod("Contains", new Type[] { typeof(string) });
+		private static MethodInfo startsWithMethod = typeof(string).GetMethod("StartsWith", new Type[] { typeof(string) });
+		private static MethodInfo endsWithMethod = typeof(string).GetMethod("EndsWith", new Type[] { typeof(string) });
+		private static MethodInfo anyMethod = typeof(ExtensibleMethods).GetMethod("AnyMatch", BindingFlags.Public | BindingFlags.Static);
 
-        private static readonly MethodInfo changeTypeMethod = typeof(Convert).GetMethod("ChangeType",new Type[] { typeof(object), typeof(Type) });
+		private static readonly MethodInfo changeTypeMethod = typeof(Convert).GetMethod("ChangeType", new Type[] { typeof(object), typeof(Type) });
 
-        public static Expression<Func<T,bool>> GetExpression<T>(IList<Filter> filters)
-        {
-            if (filters.Count == 0)
-                return null;
+		public static Expression<Func<T, bool>> GetExpression<T>(IList<Filter> filters, T t)
+		{
+			if (filters.Count == 0)
+				return null;
 
-            ParameterExpression param = Expression.Parameter(typeof(T), "t");
-            Expression exp = null;
+			ParameterExpression param = Expression.Parameter(typeof(T), "t");
+			Expression exp = null;
 
-            if (filters.Count == 1)
-                exp = GetExpression<T>(param, filters[0]);
-            else if (filters.Count == 2)
-                exp = GetExpression<T>(param, filters[0], filters[1]);
-            else
-            {
-                while (filters.Count > 0)
-                {
-                    var f1 = filters[0];
-                    var f2 = filters[1];
+			if (filters.Count == 1)
+				exp = GetExpression<T>(param, filters[0], t);
+			else if (filters.Count == 2)
+				exp = GetExpression<T>(param, filters[0], filters[1], t);
+			else
+			{
+				while (filters.Count > 0)
+				{
+					var f1 = filters[0];
+					var f2 = filters[1];
 
-                    if (exp == null)
-                        exp = GetExpression<T>(param, filters[0], filters[1]);
-                    else
-                        exp = Expression.AndAlso(exp, GetExpression<T>(param, filters[0], filters[1]));
+					if (exp == null)
+						exp = GetExpression<T>(param, filters[0], filters[1], t);
+					else
+						exp = Expression.AndAlso(exp, GetExpression<T>(param, filters[0], filters[1], t));
 
-                    filters.Remove(f1);
-                    filters.Remove(f2);
+					filters.Remove(f1);
+					filters.Remove(f2);
 
-                    if (filters.Count == 1)
-                    {
-                        exp = Expression.AndAlso(exp, GetExpression<T>(param, filters[0]));
-                        filters.RemoveAt(0);
-                    }
-                }
-            }
+					if (filters.Count == 1)
+					{
+						exp = Expression.AndAlso(exp, GetExpression<T>(param, filters[0], t));
+						filters.RemoveAt(0);
+					}
+				}
+			}
 
-            return Expression.Lambda<Func<T, bool>>(exp, param);
-        }
+			return Expression.Lambda<Func<T, bool>>(exp, param);
+		}
 
-        private static Expression Convert(Expression from, Type type)
-        {
-            return Expression.Convert(from, type);
-        }
+		private static Expression Convert(Expression from, Type type)
+		{
+			return Expression.Convert(from, type);
+		}
 
-        private static Expression GetExpression<T>(ParameterExpression param, Filter filter)
-        {
+		private static Expression GetExpression<T>(ParameterExpression param, Filter filter, T t)
+		{
+			MemberExpression member = Expression.Property(param, filter.PropertyName);
+			if (filter.Operation == Op.Contains)
+			{
+				var lamda = Expression.Lambda<Func<T, List<string>>>(member, param);
+				var cc = lamda.Compile().Invoke(t);
+			}
+			ConstantExpression constant = Expression.Constant(filter.Value);
 
-            MemberExpression member = Expression.Property(param, filter.PropertyName);
-            ConstantExpression constant = Expression.Constant(filter.Value);
 
-            
-            switch (filter.Operation)
-            {
-                case Op.Equals:
-                    return Expression.Equal(member, constant);
+			switch (filter.Operation)
+			{
+				case Op.Equals:
+					return Expression.Equal(member, constant);
 
-                case Op.GreaterThan:
-                    return Expression.GreaterThan(member, constant);
+				case Op.GreaterThan:
+					return Expression.GreaterThan(member, constant);
 
-                case Op.GreaterThanOrEqual:
-                    return Expression.GreaterThanOrEqual(member, constant);
+				case Op.GreaterThanOrEqual:
+					return Expression.GreaterThanOrEqual(member, constant);
 
-                case Op.LessThan:
-                    return Expression.LessThan(member, constant);
+				case Op.LessThan:
+					return Expression.LessThan(member, constant);
 
-                case Op.LessThanOrEqual:
-                    return Expression.LessThanOrEqual(member, constant);
+				case Op.LessThanOrEqual:
+					return Expression.LessThanOrEqual(member, constant);
 
-                case Op.Contains:
+				case Op.Contains:
+					ConstantExpression dbListConstantExpression = Expression.Constant(Expression.Lambda<Func<T, List<string>>>(member, param).Compile().Invoke(t));
+					var containsExpression = GetContainsExpression(dbListConstantExpression);
+					ParameterExpression source = Expression.Parameter(typeof(List<string>), "v");
+					var anyExpression = Expression.Call(anyMethod, source, Expression.Constant(containsExpression.Compile()));
+					var lambda = Expression.Lambda<Func<List<string>, bool>>(anyExpression, source);
+					bool status = lambda.Compile().Invoke(filter.Value);
+					return Expression.Constant(status);
+				//return Expression.Call(member, containsMethod, constant);
 
-                    return Expression.Call(member, containsMethod, constant);
-                     
-                case Op.StartsWith:
-                    return Expression.Call(member, startsWithMethod, constant);
+				case Op.StartsWith:
+					return Expression.Call(member, startsWithMethod, constant);
 
-                case Op.EndsWith:
-                    return Expression.Call(member, endsWithMethod, constant);
-            }
+				case Op.EndsWith:
+					return Expression.Call(member, endsWithMethod, constant);
+			}
 
-            return null;
-        }
+			return null;
+		}
 
-        private static BinaryExpression GetExpression<T>(ParameterExpression param, Filter filter1, Filter filter2)
-        {
-            Expression bin1 = GetExpression<T>(param, filter1);
-            Expression bin2 = GetExpression<T>(param, filter2);
+		private static BinaryExpression GetExpression<T>(ParameterExpression param, Filter filter1, Filter filter2, T t)
+		{
+			Expression bin1 = GetExpression<T>(param, filter1, t);
+			Expression bin2 = GetExpression<T>(param, filter2, t);
 
-            return Expression.And(bin1, bin2);
-        }
-    }
+			return Expression.And(bin1, bin2);
+		}
+
+		static Expression<Func<string, bool>> GetContainsExpression(ConstantExpression constant)
+		{
+			MethodInfo containsMethod = typeof(List<string>).GetMethod("Contains");
+			var uiStringParam = Expression.Parameter(typeof(string), "str");//str=>
+			Expression expContains = Expression.Call(constant, containsMethod, uiStringParam);//str=> lst.Contains(str)
+			var lamdaContains = Expression.Lambda<Func<string, bool>>(expContains, uiStringParam);
+
+			return lamdaContains;
+		}
+
+		private static object GetValue<T>(MemberExpression member)
+		{
+			var objectMember = Expression.Convert(member, typeof(T));
+
+			var getterLambda = Expression.Lambda<Func<T>>(objectMember);
+
+			var getter = getterLambda.Compile();
+
+			return getter();
+		}
+	}
+
+
 }
--- RuleTestEngine/Program.cs
+++ RuleTestEngine/Program.cs
@@ -5,80 +5,80 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
 
 namespace RuleTestEngine
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            List<string> _values = new List<string> { "abc-666","abc-222" };
-
-            List<Person> persons = new List<Person>
-            {
-                new  Person  { Name = "Flamur" ,  City = "Prishtine" ,   Salary = 12000.0, skus= _values },
-                                                                                                  
-                new  Person  { Name = "Blerta" ,  City = "Mitrovice" ,   Salary = 9000.0 , skus= _values},
-                                                                                                  
-                new  Person  { Name = "Berat" ,   City = "Peje" ,        Salary = 10000.0 , skus =_values},
-                                                                                                  
-                new  Person  { Name = "Laura" ,   City = "Mitrovice" ,   Salary = 25000.0, skus= _values},
-                                                                                                  
-                new  Person  { Name = "Olti" ,    City = "Prishtine" ,   Salary = 8000.0, skus=_values},
-                                                                                                  
-                new  Person  { Name = "Xhenis" ,  City = "Gjakove" ,     Salary = 7000.0 , skus= _values},
-                                                                                                  
-                new  Person  { Name = "Fatos" ,   City = "Peje" ,        Salary = 6000.0, skus= _values},
-
-            };
-
-            List<string> _value = new List<string> { "abc-666","abc-222" };
-            List<Filter> filter = new List<Filter>()
-            {
-                new Filter { PropertyName = "City" , Operation = Op .Equals, Value = "Peje"  },
-                new Filter { PropertyName = "Salary" , Operation = Op .GreaterThan, Value = 9000.0 },
-                new Filter { PropertyName="Skus", Operation = Op.Contains, Value=_value}
-
-            };
-
-            
-
-            var deleg = ExpressionBuilder.GetExpression<Person>(filter).Compile();
+	class Program
+	{
+		static void Main1(string[] args)
+		{
+			List<string> lstDb = new List<string> { "abc-666", "abc-222" };
+			List<string> lstUI = new List<string> { "abc-6661", "abc-222" };
+			var containsEx = GetContainsExpression(lstDb);
+			MethodInfo anyMethod = typeof(ExtensibleMethods).GetMethod("AnyMatch", BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Static);
+			ParameterExpression source = Expression.Parameter(typeof(List<string>), "v");
+			var bEXP = Expression.Call(anyMethod, source, Expression.Constant(containsEx.Compile()));
+			var lambda = Expression.Lambda<Func<List<string>, bool>>(bEXP, source);
+			bool status = lambda.Compile().Invoke(lstUI);
+		}
 
 
+		static void Main(string[] args)
+		{
+			List<string> _values = new List<string> { "abc-666", "abc-222" };
 
-            var filteredCollection = persons.Where(deleg).ToList();
-        }
+			List<Person> persons = new List<Person>
+			{
+				new  Person  { Name = "Flamur" ,  City = "Prishtine" ,   Salary = 12000.0, skus= _values },
+				new  Person  { Name = "Blerta" ,  City = "Mitrovice" ,   Salary = 9000.0 , skus= _values},
+				new  Person  { Name = "Berat" ,   City = "Peje" ,        Salary = 10000.0 , skus =_values},
+				new  Person  { Name = "Laura" ,   City = "Mitrovice" ,   Salary = 25000.0, skus= _values},
+				new  Person  { Name = "Olti" ,    City = "Prishtine" ,   Salary = 8000.0, skus=_values},
+				new  Person  { Name = "Xhenis" ,  City = "Gjakove" ,     Salary = 7000.0 , skus= _values},
+				new  Person  { Name = "Fatos" ,   City = "Peje" ,        Salary = 6000.0, skus= _values},
+			};
 
-        // Author: Cole Francis, Architect
-        /// The pre-compiled rules type
-        /// 
-        ////public class PrecompiledRules
-        ////{
-        ////    ///
-        ////    /// A method used to precompile rules for a provided type
-        ////    /// 
-        ////    public static List<Func<T, bool>> CompileRule<T>(List<T> targetEntity, List<Filter <List<string>>> rules)
-        ////    {
-        ////        var compiledRules = new List<Func<T, bool>>();
+			List<string> lstUI1 = new List<string> { "abc-666", "abc-222" };
+			List<string> lstUI2 = new List<string> { "abc-666", "abc-2221" };
+			List<string> lstUI3 = new List<string> { "abc-6661", "abc-222" };
+			List<string> lstUI4 = new List<string> { "abc-6661", "abc-2221" };
 
-        ////        // Loop through the rules and compile them against the properties of the supplied shallow object 
-        ////        rules.ForEach(rule =>
-        ////        {
-        ////            var genericType = Expression.Parameter(typeof(T));
-        ////            var key = MemberExpression.Property(genericType, rule.ComparisonPredicate);
-        ////            var propertyType = typeof(T).GetProperty(rule.ComparisonPredicate).PropertyType;
-        ////            var value = Expression.Constant(Convert.ChangeType(rule.ComparisonValue, propertyType));
-        ////            var binaryExpression = Expression.MakeBinary(rule.ComparisonOperator, key, value);
+			List<Filter> filter = new List<Filter>()
+			{
+				new Filter { PropertyName = "City" , Operation = Op .Equals, Value = "Peje"  },
+				new Filter { PropertyName = "Salary" , Operation = Op .GreaterThan, Value = 9000.0 },
 
-        ////            compiledRules.Add(Expression.Lambda<Func<T, bool>>(binaryExpression, genericType).Compile());
-        ////        });
+				new Filter { PropertyName="Skus", Operation = Op.Contains, Value=lstUI1},
+				new Filter { PropertyName="Skus", Operation = Op.Contains, Value=lstUI2},
+				new Filter { PropertyName="Skus", Operation = Op.Contains, Value=lstUI3},
+				//new Filter { PropertyName="Skus", Operation = Op.Contains, Value=lstUI4},
+			};
 
-        ////        // Return the compiled rules to the caller
-        ////        return compiledRules;
-        ////    }
-        ////}
+			var filterClone = filter.Select(v => new Filter() { PropertyName = v.PropertyName, Operation = v.Operation, Value = v.Value }).ToList();
 
+			List<Person> lstSuccessRules = new List<Person>();
+			foreach (var p in persons)
+			{
+				if (filter != null && filter.Count == 0)
+				{
+					filter = filterClone.Select(v => new Filter() { PropertyName = v.PropertyName, Operation = v.Operation, Value = v.Value }).ToList();
+				}
+				var deleg = ExpressionBuilder.GetExpression<Person>(filter, p).Compile();
+				var lstSinglePerson = new List<Person> { p };
+				var filteredCollection = lstSinglePerson.Where(deleg).ToList();
+				lstSuccessRules.AddRange(filteredCollection);
+			}
+		}
 
-    }
+		static Expression<Func<string, bool>> GetContainsExpression(List<string> lstDb)
+		{
+			MethodInfo containsMethod = typeof(List<string>).GetMethod("Contains");
+			var uiStringParam = Expression.Parameter(typeof(string), "str");//str=>
+			var dbListParam = Expression.Constant(lstDb);//passing list actual data
+			Expression expContains = Expression.Call(dbListParam, containsMethod, uiStringParam);//str=> lst.Contains(str)
+			var lamdaContains = Expression.Lambda<Func<string, bool>>(expContains, uiStringParam);
+			return lamdaContains;
+		}
+	}
 }
